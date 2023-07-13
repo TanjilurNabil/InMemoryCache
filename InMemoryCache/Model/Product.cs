fed5ff9978cb8c57113d51d93f4ef48242fa250e
Tanjilur Rahman Nabil <tanjilur.nabil@savethechildren.org>
2023-07-13 15:16:38 +0600

    Add project files.

---
+++ InMemoryCache/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace InMemoryCache.Model
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public int Stock { get; set; }
+    }
+}
